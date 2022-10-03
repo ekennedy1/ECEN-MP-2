1399bf61475a3fb8e7563218324e54075b120be1
ekennedy <ekennedy@tamu.edu>
2022-10-03 16:47:08 -0500

    multithreading fix

--- Main.java
+++ Main.java
@@ -9,11 +9,11 @@ public class Main {
 
         boolean stop = true;
         do{
-            Multithreading Thread1 = new Multithreading(0, 4);
-            Multithreading Thread2 = new Multithreading(4, 8);
-            Multithreading Thread3 = new Multithreading(8, 12);
-            Multithreading Thread4 = new Multithreading(12, 16);
-            Multithreading Thread5 = new Multithreading(16, 20);
+            Multithreading Thread1 = new Multithreading(0, 4, Matrix);
+            Multithreading Thread2 = new Multithreading(4, 8, Matrix);
+            Multithreading Thread3 = new Multithreading(8, 12, Matrix);
+            Multithreading Thread4 = new Multithreading(12, 16, Matrix);
+            Multithreading Thread5 = new Multithreading(16, 20, Matrix);
             System.out.println("Welcome to the 20 x 20 Matrix Calculator designed by Group 12.");
             System.out.println("Enter I to Initialize two new matrices, C to Calculate the matrices product, P to print the product Matrix, and Q to quit.");
             Scanner sc= new Scanner(System.in); 
@@ -34,11 +34,6 @@ public class Main {
                     Thread3.start();
                     Thread4.start();
                     Thread5.start();
-                    Matrix.multiplyMatrices(0, 4);
-                    Matrix.multiplyMatrices(4, 8);
-                    //Matrix.multiplyMatrices(8, 12);
-                    //Matrix.multiplyMatrices(12, 16);
-                    //Matrix.multiplyMatrices(16, 20);
                     break;
 
                 case "P":
@@ -46,7 +41,6 @@ public class Main {
                     
                     Matrix.printP();
                     break;
-
 
                 case "Q":
                 case "q":
--- Multithreading.java
+++ Multithreading.java
@@ -5,19 +5,17 @@ public class Multithreading extends Thread {
 
     private int rowNumber1;
     private int rowNumber2;
+    Matrices MatrixM = new Matrices();
 
     
-    public Multithreading(int rowNumber1, int rowNumber2){
+    public Multithreading(int rowNumber1, int rowNumber2, Matrices MatrixM){
         this.rowNumber1 = rowNumber1;
         this.rowNumber2 = rowNumber2;
+        this.MatrixM = MatrixM;
     }
 
     public void run() {
-        Matrices MatrixM = new Matrices();
-        System.out.print(rowNumber1);
-        System.out.print(rowNumber2);
         MatrixM.multiplyMatrices(rowNumber1, rowNumber2);
-        
 
     }
 }
